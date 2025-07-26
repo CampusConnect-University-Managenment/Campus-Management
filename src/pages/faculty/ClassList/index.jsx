4e4dee5fe044c872fb3510331a627eaeabe39d96
ICHSHANACKIYAN R <717822p317@kce.ac.in>
2025-07-26 10:22:51 +0530

    feat: Implement class list student component and route

--- src/pages/faculty/ClassList/index.jsx
+++ src/pages/faculty/ClassList/index.jsx
@@ -1,1 +1,6 @@
-export { default } from './ClassList';
+// src/pages/faculty/ClassList/index.jsx
+import ClassList from './components/ClassList.jsx';
+import ClassList_Students from './components/ClassList_Students.jsx';
+
+// Export both components using named exports
+export { ClassList, ClassList_Students };
